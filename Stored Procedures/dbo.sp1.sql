312e124af5ec3fd184ffa1df9408858f7d13e501
mikezhu <mzhu1@cpaglobal.com>
2013-07-09 12:14:11 +1000

    added sp1

---
+++ Stored Procedures/dbo.sp1.sql
@@ -0,0 +1,19 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp1]
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+END
+GO
